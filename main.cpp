294ffb476a33cc1f2fefc6c722f84ba07c8d5b8e
Sahil <guptasahilkumar783@gmail.com>
2026-03-11 02:59:28 +0530

    Clean up commented out includes in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 
 #include <bits/stdc++.h>
-//#include "Model/RubiksCube3dArray.cpp"
-//#include "Model/RubiksCube1dArray.cpp"
-//#include "Model/RubiksCubeBitboard.cpp"
 #include "Solver/DFSSolver.h"
 #include "Solver/BFSSolver.h"
 #include "Solver/IDDFSSolver.h"
 #include "Solver/IDAstarSolver.h"
-//#include "PatternDatabases/CornerPatternDatabase.h"
 #include "PatternDatabases/CornerDBMaker.h"
 
 using namespace std;
